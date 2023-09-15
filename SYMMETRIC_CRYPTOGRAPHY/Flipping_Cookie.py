d5e0d1dcd255731e2e54a173da3553ec48fa94fc
DmitriyG <garkin.dmitriy2004@gmail.com>
2023-09-15 03:36:39 +0300

    7

--- SYMMETRIC_CRYPTOGRAPHY/Flipping_Cookie.py
+++ SYMMETRIC_CRYPTOGRAPHY/Flipping_Cookie.py
@@ -14,3 +14,8 @@ def checnge_centre(c, tmp):
 
 iv = checnge_centre(iv, tmp)
 print(requests.get(f"https://aes.cryptohack.org/flipping_cookie/check_admin/{cookie}/{iv}").json()["flag"])
+
+# crypto{4u7h3n71c4710n_15_3553n714l}
+
+# inline solution:
+# [print(requests.get(f"https://aes.cryptohack.org/flipping_cookie/check_admin/{cookie[32:]}/{bytes.fromhex(cookie[:32])[:len('admin=')].hex() + ''.join([hex(bytes.fromhex(cookie[:32])[len('admin='):len('admin=') + len('True;')][i]^[b'True;'[i] ^ b'False'[i] for i in range(len(b'True;'))][i])[2:].zfill(2) for i in range(len(bytes.fromhex(cookie[:32])[len('admin='):len('admin=') + len('True;')]))]) + bytes.fromhex(cookie[:32])[len('admin=') + len('True;'):].hex()}").json()["flag"]) for cookie in [requests.get('https://aes.cryptohack.org/flipping_cookie/get_cookie/').json()['cookie']]]
